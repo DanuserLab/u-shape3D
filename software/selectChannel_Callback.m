2a5a01f2899843c4d135da9605370edcb7a982e1
Danuser CI User <danuser_ci@NucleusB107.cm.cluster>
2024-04-06 02:00:25 -0500

    Danuser Lab CI pipeline build #

--- software/selectChannel_Callback.m
+++ software/selectChannel_Callback.m
@@ -1,8 +1,6 @@
 function selectChannel_Callback(hObject, eventdata, handles)
 
 % Retrieve  channels properties
-%availableProps = get(handles.listbox_availableChannels, {'String','UserData','Value'});
-%selectedProps = get(handles.listbox_selectedChannels, {'String','UserData'});
 %
 % Copyright (C) 2024, Danuser Lab - UTSouthwestern 
 %
@@ -22,24 +20,8 @@ function selectChannel_Callback(hObject, eventdata, handles)
 % along with Morphology3DPackage.  If not, see <http://www.gnu.org/licenses/>.
 % 
 % 
-
-% Hillary Wong 3/25/2024: modified how availableProps and selectedProps
-% are initialized because the class of the handles listboxchannels were converted to different class after migrating to appdesigner. 
-%check handles.listbox_availableChannels type for tremblign correction
-%process type
-%selectedProps = {handles.listbox_selectedChannels.String [find(ismember(handles.listbox_availableChannels.String, handles.listbox_selectedChannels.String))]};
-
-if (isa(handles.listbox_availableChannels,'matlab.ui.control.ListBox'))
-    availableProps = {handles.listbox_availableChannels.Items' [1:numel(handles.listbox_availableChannels.Items)] [handles.listbox_availableChannels.Value]};
-else
-    availableProps = get(handles.listbox_availableChannels, {'String', 'UserData', 'Value'});
-end
-
-if ~isa(handles.listbox_selectedChannels,'matlab.ui.control.ListBox')
-    selectedProps = get(handles.listbox_selectedChannels, {'String', 'UserData'});
-else
-    selectedProps = {handles.listbox_selectedChannels.String [find(ismember(handles.listbox_availableChannels.String, handles.listbox_selectedChannels.String))]};
-end
+availableProps = get(handles.listbox_availableChannels, {'String','UserData','Value'});
+selectedProps = get(handles.listbox_selectedChannels, {'String','UserData'});
 
 % Find new elements and set them to the selected listbox
 newID = availableProps{3}(~ismember(availableProps{1}(availableProps{3}),selectedProps{1}));
